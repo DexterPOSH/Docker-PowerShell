7f639f68a09162f40e88d8692c60d6288a308002
DexterPOSH <dexter34925@yahoo.in>
2017-01-03 09:55:12 +0530

    Initial draft of the ShouldProcess & ShouldContinue

--- src/Docker.PowerShell/Cmdlets/RemoveContainer.cs
+++ src/Docker.PowerShell/Cmdlets/RemoveContainer.cs
@@ -1,10 +1,13 @@
 ﻿using System.Management.Automation;
+using Docker.DotNet;
 using Docker.DotNet.Models;
 using System.Threading.Tasks;
+using Docker.PowerShell.Objects;
 
 namespace Docker.PowerShell.Cmdlets
 {
     [Cmdlet(VerbsCommon.Remove, "Container",
+            SupportsShouldProcess = true,
             DefaultParameterSetName = CommonParameterSetNames.Default)]
     public class RemoveContainer : MultiContainerOperationCmdlet
     {
@@ -24,6 +27,31 @@ namespace Docker.PowerShell.Cmdlets
         {
             foreach (var id in ParameterResolvers.GetContainerIds(Container, ContainerIdOrName))
             {
+                try
+                {
+                    var container = await ContainerOperations.GetContainersByIdOrName(id, DkrClient);
+                }
+                catch
+                {
+                    // Does this module have any recommendations for catching errors ?
+                }
+                // Confirm the operation with the user first.
+                // This is always false if the WhatIf parameter is set.
+                
+                if (! ShouldProcess(string.Format(CultureInfo.CurrentCulture, "{0} - {1}", container.Names, id)))
+                {
+                    continue;
+                }
+
+                // Make sure that the user really wants to remove the container
+                if (! Force)
+                {
+                    string message = string.Format(CultureInfo.CurrentCulture, "Are you sure you wish to remove the container with name \"{0}\"?", container.Names);
+                    if (! ShouldContinue(message, "Warning!", ref yesToAll, ref noToAll))
+                    {
+                        continue;
+                    }
+                }
                 await DkrClient.Containers.RemoveContainerAsync(id,
                     new ContainerRemoveParameters() { Force = Force.ToBool() }
                     );
@@ -31,5 +59,9 @@ namespace Docker.PowerShell.Cmdlets
         }
 
         #endregion
+
+        #region Private Data
+        private bool yesToAll, noToAll;
+        #endregion Private Data
     }
 }
